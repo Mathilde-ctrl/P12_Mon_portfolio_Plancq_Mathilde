031574cfaea2e467b73fe5e4f146248099a6cf09
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-11-02 10:22:41 +0100

    404 github error

--- my-app/src/rooter.jsx
+++ my-app/src/rooter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, } from 'react-router-dom';
 
 // Import the page components
 import Projets from './Pages/Projects/projets';
